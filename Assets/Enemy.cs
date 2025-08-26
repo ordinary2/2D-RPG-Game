3873c0de6820c2eda2f2d8d30a93f413ec2eb02b
Irakli <izaridze699@gmail.com>
2025-08-27 01:46:16 +0400

    finished crash course

---
+++ Assets/Enemy.cs
@@ -0,0 +1,43 @@
+using System;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private SpriteRenderer sr;
+
+    [SerializeField] private float redColorDuration = 1;
+
+    public float currentTimeInGame;
+    public float lastTimeWasDamaged;
+    private void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        ChangeColorIfNeeded();
+    }
+
+    private void ChangeColorIfNeeded()
+    {
+        currentTimeInGame = Time.time;
+
+        if (currentTimeInGame > lastTimeWasDamaged + redColorDuration)
+        {
+            if (sr.color != Color.white)
+                sr.color = Color.white;
+        }
+    }
+
+    public void TakeDamage()
+    {
+        sr.color = Color.red;
+        lastTimeWasDamaged = Time.time;
+    }
+
+    private void TurnWhite()
+    {
+        sr.color = Color.white;
+    }
+}
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,10 +5,25 @@ public class Player : MonoBehaviour
 {
     private Animator anim;
     private Rigidbody2D rb;
+
+    [Header("Attack details")] 
+    [SerializeField] private float attackRadius;
+    [SerializeField] private Transform attackPoint;
+    [SerializeField] private LayerMask whatIsEnemy;
     
+    [Header("Movement details")]
     [SerializeField] private float moveSpeed = 3.5f;
     [SerializeField] private float jumpForce = 8;
     private float xInput;
+    private bool facingRight = true;
+    private bool canMove = true;
+    private bool canJump = true;
+
+    [Header("Collision details")] 
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask whatIsGround;
+    private bool isGrounded;
+    
 
     private void Awake()
     {
@@ -18,16 +33,34 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
+        HandleCollision();
         HandleInut();
         HandleMovement();
         HandleAnimations();
+        HandleFlip();
+    }
+
+    public void DamageEnemies()
+    {
+        Collider2D[] enemyColliders = Physics2D.OverlapCircleAll(attackPoint.position, attackRadius, whatIsEnemy);
+
+        foreach (Collider2D enemy in enemyColliders)
+        {
+            enemy.GetComponent<Enemy>().TakeDamage();
+        }
+    } 
+
+    public void EnableMovementAndJump(bool enable)
+    {
+        canMove = enable;
+        canJump = enable;
     }
 
     private void HandleAnimations()
     {
-        bool isMoving = rb.linearVelocity.x != 0;
-        
-        anim.SetBool("isMoving", isMoving);
+        anim.SetFloat("xVelocity", rb.linearVelocity.x);
+        anim.SetFloat("yVelocity", rb.linearVelocity.y);
+        anim.SetBool("isGrounded", isGrounded);
     }
 
     private void HandleInut()
@@ -36,17 +69,63 @@ public class Player : MonoBehaviour
         
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Jump();
+            TryToJump();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            TryToAttack();
         }
     }
 
+    private void TryToAttack()
+    {
+        if (isGrounded)
+        {
+            anim.SetTrigger("attack");
+        }
+    }
+    
+    private void TryToJump()
+    {
+        if(isGrounded && canJump)
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+    }
+    
     private void HandleMovement()
     {
-        rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocity.y);
+        if(canMove)
+            rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocity.y);
+        else
+            rb.linearVelocity = new Vector2(0, rb.linearVelocity.y);
     }
 
-    private void Jump()
+    private void HandleCollision()
     {
-        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+    }
+
+    private void HandleFlip()
+    {
+        if (rb.linearVelocity.x > 0 && facingRight == false)
+        {
+            Flip();
+        }
+        else if (rb.linearVelocity.x < 0 && facingRight == true)
+        {
+            Flip();
+        }
+    }
+    
+    private void Flip()
+    {
+        transform.Rotate(0, 180, 0);
+        facingRight = !facingRight;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, transform.position + new Vector3(0, -groundCheckDistance));
+        Gizmos.DrawWireSphere(attackPoint.position, attackRadius);
     }
 }
